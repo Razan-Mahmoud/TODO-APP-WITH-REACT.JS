81516d95a1eaaa89083098a195f76a8433a96896
Razan M. Fahmi <razan_assem@hotmail.com>
2024-12-12 16:45:57 +0200

    add: make icons accessible manually

--- src/Components/TodoCard.jsx
+++ src/Components/TodoCard.jsx
@@ -7,14 +7,14 @@ export default function TodoCard(props) {
     <li className='todoLi'>
       {children}
       <div className='divIcon'>
-        
+
         <i onClick={() => {
           editTodo(index)
-        }} className="fa-solid fa-pen-to-square icon"></i>
+        }} className="fa-solid fa-pen-to-square icon" aria-hidden="true"></i><span class="fa-sr-only">Edit Icon</span>
 
         <i onClick={() => {
           deleteTodo(index)
-        }} className="fa-solid fa-trash-can icon"></i>
+        }} className="fa-solid fa-trash-can icon" aria-hidden="true"></i><span class="fa-sr-only">Delete Icon</span>
       </div>
     </li>
   )
