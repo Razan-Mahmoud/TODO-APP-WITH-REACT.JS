b46c1a142944b1ecf1b9118fbefb6a784023356f
Razan M. Fahmi <razan_assem@hotmail.com>
2024-12-12 16:14:05 +0200

    add: fontawesome config

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from 'react'
 import TodoInput from './Components/TodoInput'
 import TodoList from './Components/TodoList'
 import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
 
 export default function App() {
 
